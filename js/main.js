1cc6018c01daba5173af1b309a47c0fd046dee71
Lesther Reynoso <reynosl@sunyit.edu>
2014-05-22 10:02:17 -0400

    site still needs work but info is displayed

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
 $(document).ready(function(){
+        
         $('.carousel').carousel({
-          interval: false
+
+          	interval: false
+        
         })
+
         $("div.panel-heading > ul.nav > li").click(function(){
-          $("div.panel-heading > ul.nav > li").removeClass("active")
-          $(this).addClass("active")
+
+        	$("div.panel-heading > ul.nav > li").removeClass("active").attr("id", "")
+        	$(this).addClass("active").attr("id","active")
+
         })
-    })
+        
+        $("div.panel-heading > ul.nav > li > a").click(function(){
+
+        	$("div.panel-heading > ul.nav > li > a").removeClass("active")
+        	$(this).addClass("active")
+  		
+  		})
+})
